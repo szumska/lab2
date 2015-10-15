3c56ca51db79a971d6d6c9138768829b75017fcd
mianowski <g.mianowski@gmail.com>
2015-10-16 01:41:23 +0200

    Dodałem treść zadania i plik będący poligonem doświadczalnym

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # lab2
 Rekomendacja metodą najbliższego sąsiada
+
+## Zadania
+  1. Zapoznaj się ze strukturą słownika w języku python
+  https://pl.wikibooks.org/wiki/Zanurkuj_w_Pythonie/S%C5%82owniki
+  https://pl.python.org/docs/tut/node7.html - punkt "Słowniki"
+  Poeksperymentuj ze słownikiem users (wyświetlanie, filtrowanie, dodawanie elementów, usuwanie elementów)
+  
+  2. Zapoznaj się z pojęciem odległości taksówkowej
+  https://pl.wikipedia.org/wiki/Przestrzeń_metryczna
+  Zaimplementuj funkcję manhattan. Potrzebna będzie konstrukcja pętli "for ... in ...", instrukcja warunkowa "if ... else ..." oraz funkcja abs()
+  
+  3. Poznaj listy (https://pl.wikibooks.org/wiki/Zanurkuj_w_Pythonie/Listy) oraz sposoby ich sortowania (https://pl.python.org/forum/index.php?topic=853.0). Następnie zaimplementuj funkcję computeNearestNeighbor
+  
+  4. Poznaj możliwości funkcji sorted() (https://wiki.python.org/moin/HowTo/Sorting). Później zaimplementuj funkcję recommend
+  
+  5. Zgłoś swoje rozwiązanie jako pull request do repozytorium eksploracjaUp2015/lab2
---
+++ rekomendacja.py
@@ -0,0 +1,48 @@
+﻿#  W    zorowane na przykładzie Rona Zacharskiego
+
+
+from math import sqrt
+
+users = {"Ania": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones": 4.5, "Phoenix": 5.0, "Slightly Stoopid": 1.5, "The Strokes": 2.5, "Vampire Weekend": 2.0},
+         "Bonia":{"Blues Traveler": 2.0, "Broken Bells": 3.5, "Deadmau5": 4.0, "Phoenix": 2.0, "Slightly Stoopid": 3.5, "Vampire Weekend": 3.0},
+         "Celina": {"Blues Traveler": 5.0, "Broken Bells": 1.0, "Deadmau5": 1.0, "Norah Jones": 3.0, "Phoenix": 5, "Slightly Stoopid": 1.0},
+         "Dominika": {"Blues Traveler": 3.0, "Broken Bells": 4.0, "Deadmau5": 4.5, "Phoenix": 3.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 2.0},
+         "Ela": {"Broken Bells": 4.0, "Deadmau5": 1.0, "Norah Jones": 4.0, "The Strokes": 4.0, "Vampire Weekend": 1.0},
+         "Fruzia":  {"Broken Bells": 4.5, "Deadmau5": 4.0, "Norah Jones": 5.0, "Phoenix": 5.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 4.0},
+         "Gosia": {"Blues Traveler": 5.0, "Broken Bells": 2.0, "Norah Jones": 3.0, "Phoenix": 5.0, "Slightly Stoopid": 4.0, "The Strokes": 5.0},
+         "Hela": {"Blues Traveler": 3.0, "Norah Jones": 5.0, "Phoenix": 4.0, "Slightly Stoopid": 2.5, "The Strokes": 3.0}
+        }
+
+
+
+def manhattan(rating1, rating2):
+    """Oblicz odległość w metryce taksówkowej między dwoma  zbiorami ocen
+       danymi w postaci: {'The Strokes': 3.0, 'Slightly Stoopid': 2.5}
+       Zwróć -1, gdy zbiory nie mają wspólnych elementów"""
+       
+    # TODO: wpisz kod
+    pass
+
+
+def computeNearestNeighbor(username, users):
+    """dla danego użytkownika username, zwróć ze słownika users nazwę użytkownika o najbliższych preferencjach"""
+    nameOfNearestNeighbor = ""
+    distances = []
+    # TODO: wpisz kod
+    return nameOfNearestNeighbor
+
+def recommend(username, users):
+    """Zwróć listę rekomendacji dla użytkownika"""
+    # znajdź preferencje najbliższego sąsiada
+    nearest = computeNearestNeighbor(username, users)
+    
+    recommendations = []
+    # zarekomenduj użytkownikowi wykonawcę, którego jeszcze nie ocenił, a zrobił to jego najbliższy sąsiad
+    # TODO: wpisz kod
+    
+    return sorted(recommendations, key=lambda artistTuple: artistTuple[1], reverse = True)
+
+# przykłady
+
+print( recommend('Hela', users))
+#print( recommend('Celina', users))
